39d99245e9ce565b4ec1a47fb53bd8f978f4f47e
H M Talha <hmttalha4@gmail.com>
2021-10-28 10:30:34 +0500

    Javascripts

---
+++ app.js
@@ -0,0 +1,257 @@
+// ------chap# Alert------
+// ------ques#1
+// alert("HELLO SIR....!")
+
+
+//------ques#2
+//alert("Eror! Please Enter A Valid Password")
+
+
+//------ques#3
+// var line1 = "Welcome To Js Land"; 
+// var line2 = "Happy Coding";
+// var message = line1 + "\n" + line2
+// alert(message)
+
+//------ques#4
+//alert("Welcome To Js Land")
+//var line1 = "Happy Coding";
+//var line2 = "Prevent this page from creating additional dialogs";
+//var message = line1 + "\n" + line2;
+//alert(message)
+
+
+//------ques#5
+// alert("Hello...! I can run JS through my web browser console")
+
+
+
+
+// ----------Chap# Variables-------
+//------ques#1
+//var username;
+
+
+//------ques#2
+//var myName = "Hafiz Muhammad Talha Shahtaj"
+
+
+//------ques#3
+// var message = "Hello World...!"
+// alert(message)
+
+
+//------ques#4
+// var name = "JHone Doe";
+// var age = 15;
+// var age1 = "years old"
+// var fullAge = age + " " + age1
+// var course = "certified Mobile application development"
+// alert(name)
+// alert(fullAge)
+// alert(course)
+
+
+//------ques#5
+// var pizza1 = "PIZZA"
+// var pizza2 = "PIZZ"
+// var pizza3 = "PIZ"
+// var pizza4 = "PI"
+// var pizza5 = "P"
+
+// var allPizza = pizza1 + "\n" + pizza2 + "\n" + pizza3 + "\n" + pizza4 + "\n" + pizza5 ;
+
+// alert(allPizza)
+
+
+//------ques#6
+// var email = "hmttalha4@gmail.com"
+// alert(email)
+
+
+//------ques#7
+// var book = "A smarter way to learn \n JavaScript";
+// alert(book)
+
+
+//------ques#8
+// document.write("Yuh I can Write HTML content through JAVASCRIPt")
+
+
+//------ques#9
+// var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(design)
+
+
+
+//------------Chap# Number VAriables------
+//------ques#1
+// var age = "I Am 15 Years Old"
+// alert(age)
+
+
+//------ques#2
+// var track ="you have visited 14 times"
+// alert(track)
+
+
+//------ques#3
+// var birthYear = 2000
+// document.write(" My Brth Year Is " + birthYear)
+// document.write("<br>")
+// document.write("data type mof my variable is number")
+
+
+//------ques#3
+// var visitorsName ="John Doe"
+// var productTittle = "Shirt"
+// var quantity = "5 T-shirts"
+// document.write("“John Doe ordered 5 T-shirt(s) on XYZ Clothing store")
+
+
+
+//-----Chap# VARIABLE NAMES: LEGAL & ILLEGAL---------
+//------ques#1
+// var name = "talha" , age = 21 , weight = 70;
+
+
+//------ques#2
+//var full name= "talha";    illegal1
+// var var = "sa"d;          illegal2
+// var name-full = "mkl";    illegal3
+// var 1name = "Talha"       illegal4
+// var !name = "talha"       illegal5  
+
+// var  name = "talha";       legal1
+// var name1 = "talha"l       legal2
+// var fullNAme = "HM TALHA"  legal3
+// var naem_as = "talha"      legal4 
+// var $name = talha          legal5 
+
+
+//------ques#3
+// document.write("<h1>Rules for naming JS variables</h1>")
+// document.write(" Variable names can only contain neumbers , and__ and for example smy _1st_variable. ,<br>")
+// document.write("Variables must begin with a ______, ______ or_____. For example $name, _name or name<br>")
+// document.write(" Variable names are case sensitve<br>")
+// document.write("Variable names should not be JS KEyword")
+
+ 
+// -----Math Expressions-----
+//-----Ques#1
+// var num1 = 3
+// var num2 = 5
+// var num3 = num1 + num2
+// document.write("Sum of 3 and 5 is" + " " +num3)
+
+
+// -----Ques#2
+// var num1 = 3
+// var num2 = 5
+// var num3 = num1 - num2
+// document.write("Sum of 3 and 5 is" + " " +num3)
+
+// var num1 = 3
+// var num2 = 5
+// var num3 = num1 / num2
+// document.write("Sum of 3 and 5 is" + " " +num3)
+
+// var num1 = 3
+// var num2 = 5
+// var num3 = num1 * num2
+// document.write("Sum of 3 and 5 is" + " " +num3)
+
+
+// -----Ques#3
+// var num = 3
+// document.write("The value is"+ " "+ num + "<br>")
+// var initializeValue = 5
+// document.write("The initial value is"+" "+ initializeValue + "<br>")
+// var increement = ++initializeValue
+// document.write("Value after increement is" + " " +increement + "<br>")
+// var add = increement + 7
+// document.write("Value after addition" + " " +add + "<br>")
+// var decreement = --add
+// document.write("Value after Decreement" + " " +decreement + "<br>")
+// var percentage = 12 % 3
+// document.write("Value after remainder" + " " +percentage + "<br>")
+
+
+// -----Ques#4
+// var filmTicket = 600 * 5
+// document.write("Total cost to buy 5 tickets is"+" "+ filmTicket + "PKR")
+
+
+// -----Ques#5
+//-----table
+// document.write("2 x 1 = 2 " + "<br>")
+// document.write("2 x 2 = 4 " + "<br>")
+// document.write("2 x 3 = 6 " + "<br>")
+// document.write("2 x 4 = 8 " + "<br>")
+// document.write("2 x 5 = 10 " + "<br>")
+// document.write("2 x 6 = 12 " + "<br>")
+// document.write("2 x 7 = 14 " + "<br>")
+// document.write("2 x 8 = 16 " + "<br>")
+// document.write("2 x 9 = 18 " + "<br>")
+// document.write("2 x 10 = 20 " + "<br>")
+
+
+// -----Ques#6
+// var c =222
+// var f = 333
+// var celcius = (f-32) * 5/9;
+// var fahranite = (c*9/5) + 32;
+// document.write(celcius + "C " + "Is" + fahranite + "F" + "<br>")
+
+// var c =212
+// var f = 353
+// var celcius = (f-32) * 5/9;
+// var fahranite = (c*9/5) + 32;
+// document.write( fahranite + "F" + "Is" +celcius + "C " )
+
+
+// -----Ques#8
+//....MarkSheet
+// var name = prompt("Enter Your Name");
+// var age = prompt("Enter Your Age")
+// var schoolName = prompt("Enter Your School Name");
+// var classs = prompt("Enter Your class");
+// var rollNumber = prompt("Enter Your roll number");
+// var english = prompt("Enter Your English Marks");
+// var urdu = prompt("Enter Your Urdu Marks");
+// var islamiat = prompt("Enter Your Islamiat Marks");
+// var math = prompt("Enter Your Maths Marks");
+// var science = prompt("Enter Your Science Marks");
+// var obtainedMarks = parseInt(english) + parseInt(urdu) + parseInt(islamiat) + parseInt(math) + parseInt(science); 
+// var percentage = obtainedMarks * 100 / 500 ;
+
+// document.write("Name:" + " " + name + "<br>");
+// document.write("Age:" + " " + age + "<br>");
+// document.write("School Name:" + " " + schoolName + "<br>");
+// document.write("Class:" + " " + classs + "<br>");
+// document.write("Roll Number:" + " " + rollNumber + "<br>");
+// document.write("Obtained Marks:" + " " + obtainedMarks + "<br>");
+// document.write("Percentage:" + " " + percentage + "<br>");
+
+
+
+// -----Ques#9
+// var usdIntoPkr = 10 * 104.80
+// var riyalIntoPkr = 25 * 28 
+// var total = usdIntoPkr + riyalIntoPkr
+// document.write("total Currency INto pkr IS"+" "+total)
+
+
+// -----Ques#10
+// var number = 10;
+// var arthmetic = (number + 5 * 10) / 2
+// alert(arthmetic)
+
+
+// -----Ques#11
+// var currentYear = +prompt("Enter current year")
+// var dateOfBirth = +prompt("Enter date of birth")
+// document.write("Current year is"+" "+currentYear+"<br>")
+// document.write("Your Date of Birth  is"+" "+dateOfBirth+"<br>")
+// document.write("Your age  is"+" ")
+// document.write(currentYear - dateOfBirth)
